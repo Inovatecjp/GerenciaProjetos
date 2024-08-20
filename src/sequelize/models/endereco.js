aac893aa3c118f30f50d9e6dc7fdbe75287ed4ad
josehenriquefs <devjosehenriquefs@gmail.com>
2024-08-20 13:16:01 -0300

    Fix: comenta temporariamente o código

--- src/sequelize/models/endereco.js
+++ src/sequelize/models/endereco.js
@@ -1,27 +1,33 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class endereco extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  endereco.init({
-    id: DataTypes.UUIDV4
-  }, {
-    sequelize,
-    modelName: 'endereco',
-  });
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class endereco extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   endereco.init({
+//     id: DataTypes.UUIDV4,
+//     rua: DataTypes.STRING,
+//     cep: DataTypes.STRING,
+//     bairro: DataTypes.STRING,
+//     uf: DataTypes.STRING,
+//     complemento: DataTypes.STRING,
 
-  endereco.associate = models => {
-    endereco.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
-  }
-  return endereco;
-};
+//   }, {
+//     sequelize,
+//     modelName: 'endereco',
+//   });
+
+//   endereco.associate = models => {
+//     endereco.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+//   }
+//   return endereco;
+// };
