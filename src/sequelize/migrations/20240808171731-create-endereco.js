22129b1bfc3795b5713468188dde177b7726668e
josehenriquefs <devjosehenriquefs@gmail.com>
2024-08-20 13:14:13 -0300

    Updt: modifica comportamento

--- src/sequelize/migrations/20240808171731-create-endereco.js
+++ src/sequelize/migrations/20240808171731-create-endereco.js
@@ -1,51 +1,51 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('enderecos', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      rua:{
-        type: Sequelize.STRING,
-      },
-      cep:{
-        type: Sequelize.STRING,
-      },
-      logradouro:{
-        type: Sequelize.STRING,
-      },
-      bairro:{
-        type: Sequelize.STRING,
-      },
-      uf:{
-        type: Sequelize.STRING,
-      },
-      ddd:{
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      endereco_id: {
-        type: Sequelize.UUIDV4,
-        references: {
-          model: 'Users',
-          key: 'id'
-        },
-        allowNull: false
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('enderecos');
-  }
-};
+// 'use strict';
+
+// const { DataTypes } = require('sequelize');
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.createTable('enderecos', {
+//       id: {
+//         defaultValue: DataTypes.UUIDV4,
+//         allowNull: false,
+//         primaryKey: true,
+//         type: Sequelize.UUIDV4
+//       },
+//       rua:{
+//         type: Sequelize.STRING,
+//       },
+//       cep:{
+//         type: Sequelize.STRING,
+//       },
+//       bairro:{
+//         type: Sequelize.STRING,
+//       },
+//       uf:{
+//         type: Sequelize.STRING,
+//       },
+//       complemento: {
+//         type: Sequelize.STRING
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       user: {
+//         type: Sequelize.UUIDV4,
+//         references: {
+//           model: 'Users',
+//           key: 'id'
+//         },
+//         allowNull: false
+//       }
+//     });
+//   },
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.dropTable('enderecos');
+//   }
+// };
